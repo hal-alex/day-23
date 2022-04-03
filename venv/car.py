a1182f6657b9d563976066c8c987039f66907855
Alex H <102769562+hal-alex@users.noreply.github.com>
2022-04-03 22:17:48 +0100

    Update car.py

--- venv/car.py
+++ venv/car.py
@@ -7,12 +7,10 @@ class Car(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.penup()
         self.shape("square")
+        self.penup()
         self.shapesize(stretch_wid=1, stretch_len=2)
-        
         random_color = random.choice(COLORS)
         self.color(random_color)
-
         random_y_pos = random.randint(-250, 250)
-        self.goto(x = 300, y = random_y_pos)
+        self.goto(x=300, y=random_y_pos)
