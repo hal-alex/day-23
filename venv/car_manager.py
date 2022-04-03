5e66eed04d3d82a41caca07bf301d04a71cfd632
Alex H <102769562+hal-alex@users.noreply.github.com>
2022-04-03 22:18:15 +0100

    Update car_manager.py

--- venv/car_manager.py
+++ venv/car_manager.py
@@ -1,18 +1,27 @@
+import random
 from turtle import Turtle
 from car import Car
+from random import randint
 
 STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+MOVE_INCREMENT = 5
 
-class CarManager():
+class CarManager(Turtle):
 
     def __init__(self):
+        super().__init__()
         self.all_cars_list = []
+        self.speed_of_car = STARTING_MOVE_DISTANCE
 
     def create_a_car(self):
-        car = Car()
-        self.all_cars_list.append(car)
+        random_number = random.randint(1, 10)
+        if random_number <=5:
+            car = Car()
+            self.all_cars_list.append(car)
 
     def move_all_cars(self):
         for car in self.all_cars_list:
-            car.backward(STARTING_MOVE_DISTANCE)
+            car.backward(self.speed_of_car)
+
+    def next_level(self):
+        self.speed_of_car += MOVE_INCREMENT
